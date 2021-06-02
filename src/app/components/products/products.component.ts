ac16bea3496701c1991a23645f4fefc71b78864d
stambouli <aminestambouli90@yahoo.fr>
2021-06-02 23:14:17 +0200

    Create NavBar products filter

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/products.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products:Product[] | null=null;
 
-  constructor() { }
+  constructor(private productService: ProductsService) { }
 
   ngOnInit(): void {
+  }
+  onGetAllProducts() {
+    this.productService.getAllProducts().subscribe(data => {
+      this.products=data;
+    })
+
   }
 
 }
